3b2698e3fdc608a961035b5d3f660e3958e0ebd4
Stitch-56 <stitch11497@hotmail.com>
2019-01-29 00:02:36 +0000

    TrapDoor Attempt

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    public HingeJoint2D pivotTrapDoor;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        CheckForKeyboardInputs();
+	}
+
+    private void CheckForKeyboardInputs()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            JointMotor2D theMotor = pivotTrapDoor.motor;
+            theMotor.motorSpeed = 100f;
+            pivotTrapDoor.motor = theMotor;
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            JointMotor2D theMotor = pivotTrapDoor.motor;
+            theMotor.motorSpeed = -100f;
+            pivotTrapDoor.motor = theMotor;
+        }
+    }
+}
---
+++ Assets/TrapTriggerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapTriggerController : MonoBehaviour {
+    public LevelManager levelManager;
+    // Use this for initialization
+    void Start ()
+    {
+        levelManager = FindObjectOfType<LevelManager>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        public void OnTriggerEnter2D(Collider2D collision)
+        //this function allows us to set the checkpoints we have to the transform we made and send a debug log to the console to show activation.
+        //it activates the checkpoint through collisions
+        {
+            levelManager.currentCheckpoint = gameObject;
+            Debug.Log("Activated Checkpoint" + transform.position);
+        }
+    }
+}
